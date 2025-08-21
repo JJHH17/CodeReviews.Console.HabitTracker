06039d182f7e43d7fd9b465e6cbad2091d3a5d74
James <james.hatfield17@icloud.com>
2025-08-21 22:24:26 +0100

    Initial commit, created program.cs, which includes the main program, a habit class, and a database class

---
+++ HabitLogger.JJHH17/HabitLogger.JJHH17/Program.cs
@@ -0,0 +1,23 @@
+﻿// Main program class
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello from Main");
+    }
+}
+
+// Habit class
+class Habit
+{
+    public string name { get; set; }
+    public int quantity { get; set; }
+    public string date { get; set; }
+
+    public Habit(string name, int quantity, string date)
+    {
+        this.name = name;
+        this.quantity = quantity;
+        this.date = date;
+    }
+}
