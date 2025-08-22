13bc59a8d02a3b753892d724004de97319acdb6a
James <james.hatfield17@icloud.com>
2025-08-22 07:10:03 +0100

    Added database class constructor. Creates a sqlite db if one doesn't exist

--- HabitLogger.JJHH17/HabitLogger.JJHH17/Program.cs
+++ HabitLogger.JJHH17/HabitLogger.JJHH17/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Main program class
+using Microsoft.Data.Sqlite;
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello from Main");
+        Database db = new Database();
+
     }
 }
 
@@ -19,5 +23,28 @@ class Habit
         this.name = name;
         this.quantity = quantity;
         this.date = date;
+    }
+}
+
+// Database class
+class Database
+{
+    // Constructor should create a db if one doesn't exist
+    public Database()
+    {
+        try
+        {
+            // Creates db in bin/debug/net7.0
+            using (var connection = new SqliteConnection("Data Source=test.db"))
+            {
+                connection.Open();
+                Console.WriteLine("Database opened");
+                connection.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error: " + e.Message);
+        }
     }
 }
